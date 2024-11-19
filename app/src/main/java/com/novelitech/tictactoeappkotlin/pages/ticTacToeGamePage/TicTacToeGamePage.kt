8fc4c91d39a0bb87f92f692ee5ebc7ad3aab07b3
Bruno Noveli <brunimzuppwolf@gmail.com>
2024-11-18 23:14:51 -0300

    Added snackbar to communicate victory/draw from the round

--- app/src/main/java/com/novelitech/tictactoeappkotlin/pages/ticTacToeGamePage/TicTacToeGamePage.kt
+++ app/src/main/java/com/novelitech/tictactoeappkotlin/pages/ticTacToeGamePage/TicTacToeGamePage.kt
@@ -1,24 +1,41 @@
 package com.novelitech.tictactoeappkotlin.pages.ticTacToeGamePage
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.novelitech.tictactoeappkotlin.core.validations.GameValidation
 import com.novelitech.tictactoeappkotlin.models.Player
 import com.novelitech.tictactoeappkotlin.models.Position
 import com.novelitech.tictactoeappkotlin.pages.ticTacToeGamePage.widgets.board.Board
 import com.novelitech.tictactoeappkotlin.pages.ticTacToeGamePage.widgets.display.Display
+import kotlinx.coroutines.launch
 
 /**
  * I had some issues with the update of the board using the mutableState in a collection.
@@ -27,6 +44,7 @@ import com.novelitech.tictactoeappkotlin.pages.ticTacToeGamePage.widgets.display
  * The links above explain why it happens
  */
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TicTacToeGamePage(modifier: Modifier = Modifier) {
 
@@ -61,11 +79,49 @@ fun TicTacToeGamePage(modifier: Modifier = Modifier) {
         }
     }
 
+    // Control the snackBar
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+
     Scaffold(
-        modifier = modifier
+        modifier = modifier,
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        },
+        topBar = {
+//            TopAppBar(
+//                modifier = Modifier.background(Color.Blue),
+//                title = {
+//                    Text(
+//                        text = "Tic Tac Toe",
+//                    )
+//                },
+//            )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.Blue),
+                contentAlignment = Alignment.Center,
+            ) {
+                Box(
+                    modifier = Modifier
+                        .padding(top = 48.dp, bottom = 28.dp)
+
+                ) {
+                    Text(
+                        text = "Tic Tac Toe",
+                        style = TextStyle(
+                            fontSize = 32.sp,
+                            fontWeight = FontWeight.Bold,
+                        )
+                    )
+                }
+            }
+        }
     ) { innerPadding ->
         Column(
-            modifier = Modifier.padding(innerPadding)
+            modifier = Modifier
+                .padding(innerPadding)
         ) {
             Display(
                 currentPlayer = currentPlayer,
@@ -87,19 +143,27 @@ fun TicTacToeGamePage(modifier: Modifier = Modifier) {
                         if(GameValidation.isWinner(board, currentPlayer)) {
                             currentScore[currentPlayer] = currentScore[currentPlayer]!! + 1
 
-                            // Present message about the winner and restart board after 2 seconds
-                            cleanBoard()
+                            scope.launch {
+                                snackbarHostState.showSnackbar(
+                                    "The player $currentPlayer won the game.",
+                                    withDismissAction = true,
+                                )
+
+                                cleanBoard()
+                            }
                         } else if(GameValidation.isEveryBoxFulfilled(board)) {
 
-                            // It was a draw
-                            // Present message about the draw and restart board after 2 seconds
-                            cleanBoard()
+                            scope.launch {
+                                snackbarHostState.showSnackbar(
+                                    "Game ended up as a draw",
+                                    withDismissAction = true,
+                                )
+
+                                cleanBoard()
+                            }
                         }
 
                         changeCurrentPlayer()
-
-//                        println("Clicked position: x: ${position.x} | y: ${position.y}")
-//                        println("Current board: $board")
                     }
                 }
             )
